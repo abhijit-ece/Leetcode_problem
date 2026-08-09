e7155aebdd0e98475661d75662ac415d3ffabe02
Abhijit k <abhijitkumaresan@gmail.com>
2026-08-09 16:57:41 +0530

    feat: Add Stone Game II

--- Array/Stone Game II/java/Solution.java
+++ Array/Stone Game II/java/Solution.java
@@ -1,32 +1,42 @@
 class Solution {
-    public int stoneGameII(int[] piles) {
-    }
-}class Solution {
     int[][] dp;
     int[] suffix;
     int n;
+
     public int stoneGameII(int[] piles) {
         n = piles.length;
+
         dp = new int[n][n + 1];
         suffix = new int[n + 1];
+
+        // Calculate suffix sum
         for (int i = n - 1; i >= 0; i--) {
             suffix[i] = suffix[i + 1] + piles[i];
         }
+
         return solve(0, 1, piles);
     }
+
     int solve(int i, int m, int[] piles) {
+
         if (i >= n) {
             return 0;
         }
+
         if (dp[i][m] != 0) {
             return dp[i][m];
         }
+
         if (i + 2 * m >= n) {
             return dp[i][m] = suffix[i];
         }
+
         int best = 0;
+
         for (int x = 1; x <= 2 * m; x++) {
-            int current = suffix[i] - solve(i + x, Math.max(m, x), piles);
+
+            int current = suffix[i]
+                    - solve(i + x, Math.max(m, x), piles);
 
             best = Math.max(best, current);
         }
